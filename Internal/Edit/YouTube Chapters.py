15344560d9296578088a5f67a857b79df3d16aed
Jeff Sawdon <jjsawdon+github@gmail.com>
2023-04-10 21:36:33 -0700

    Initial Commit
    Python3 script to generate a .txt file for YouTube chapter markers on the user's desktop.

---
+++ Internal/Edit/YouTube Chapters.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+## IMPORTS ##
+import os
+#import DaVinciResolveScript as bmd
+from pathlib import Path
+
+# Set Basic Resolve Settings
+dvr = bmd.scriptapp("Resolve")
+fusion = dvr.Fusion()
+projectManager = dvr.GetProjectManager()
+project = projectManager.GetCurrentProject()
+timeline = project.GetCurrentTimeline()
+
+# Get Info From Resolve
+framerate = timeline.GetSetting('timelineFrameRate')
+markers = timeline.GetMarkers()
+timelineName = timeline.GetName()
+
+# Set File Path
+chapterList = (timelineName + '.txt')
+desktopPath = os.path.join(str(Path.home()), 'Desktop')
+writePath = os.path.join(desktopPath, chapterList)
+
+# Filter Markers
+filtered_dict = {k:v for k,v in markers.items() if v['color'] == 'Blue'}
+formatted_dict = {f'{int(k//(framerate*3600)):02d}:{int(((k//framerate)%60))%60:02d}:{int((k//framerate)//60):02d}:{str(int(k%60)).zfill(2)}.{int((k%1)*framerate):02d}' if isinstance(framerate, float) else f'{int(k//(framerate*3600)):02d}:{int(((k//framerate)%60))%60:02d}:{int((k//framerate)//60):02d}:{str(int(k%60)).zfill(2)}.{int(framerate):02d}':v for k,v in filtered_dict.items()}
+
+
+with open(writePath, 'w') as f:
+	for k,v in formatted_dict.items():
+		f.write(f'Chapter: {v["name"]}, Start Time: {k}\n')
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # DaVinci Resolve Utilities
  Utilities for DaVinci Resolve
+
+ A collection of Python scripts for DaVinci Resolve
+
+## YouTube Chapter Markers
+
+***Install Instructions***
+
+**macOS**
+
+Copy `Edit/YouTube Chapters.py` to '~/Library/Application Support/Blackmagic Design/DaVinci Resolve/Fusion/Scripts/Edit/'
+
+***Usage***
+
+From Workspace>Scripts (or Workspace>Scripts>Edit), select "YouTube Chapters" to generate a .txt file with YouTube chapter markers to your desktop.
+
+To run this externally, make sure Resolve is running, and run `python3 yt_chapters_external.py` in a terminal/command prompt.
+
+***Known Issues***
+
+Currently only pulls blue markers
